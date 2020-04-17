1bb679248439e462aea2e63efb20458d5334c68e
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-17 21:07:10 +0300

    ch1

--- MyNewAppLesson/Form1.Designer.cs
+++ MyNewAppLesson/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(217, 14);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "OK";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(12, 19);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(84, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Как вас зовут?";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(102, 16);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 20);
+            this.tb1.TabIndex = 2;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(317, 16);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 13);
+            this.lbl2.TabIndex = 3;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(217, 54);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "Clear";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btn2;
     }
 }
 
--- MyNewAppLesson/Form1.cs
+++ MyNewAppLesson/Form1.cs
@@ -16,5 +16,25 @@ namespace MyNewAppLesson
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if(tb1.Text != null)
+            {
+                lbl2.Text = "ПРИВЕТ " + tb1.Text;
+                tb1.Clear();
+            }
+            else
+            {
+                lbl2.Text = "Введите текст";
+                lbl2.Text = "";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            tb1.Text = "";
+            lbl2.Text = "";
+        }
     }
 }
