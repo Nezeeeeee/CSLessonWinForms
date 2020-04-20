c229f66823d1ff12d048510839bd930333f1e874
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-20 17:46:01 +0300

    ch3

--- ThreadLesson/Program.cs
+++ ThreadLesson/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadLesson
 {
@@ -10,6 +11,20 @@ namespace ThreadLesson
     {
         static void Main(string[] args)
         {
+            //Получаем текущий поток
+            Thread t = Thread.CurrentThread;
+
+            //Получаем имя потока
+            Console.WriteLine($"Имя потока: {t.Name}");
+            t.Name = "Метод Main";
+            Console.WriteLine($"Имя потока: {t.Name}");
+
+            Console.WriteLine($"Запущен ли поток: {t.IsAlive}");
+            Console.WriteLine($"Приоритет потока: {t.Priority}");
+            Console.WriteLine($"Статус потока: {t.ThreadState}");
+
+            //Получаем домен приложения
+            Console.WriteLine($"Домен приложения: {Thread.GetDomain().FriendlyName}");
         }
     }
 }
