017ef1d8dc26753516723e2029cb0dc9721f5493
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-23 19:57:32 +0300

    Потоки с параметрами и ParameterizedThreadStart

---
+++ ParameterizedThreadStart/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterizedThreadStart
+{
+    class Program
+    {
+        //Потоки с параметрами и ParameterizedThreadStart
+        static void Main(string[] args)
+        {
+        }
+    }
+}
