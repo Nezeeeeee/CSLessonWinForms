c0f1531ff61e9313e8ad76584b61f96a19689027
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-23 20:09:19 +0300

    поток с одним параметром

--- ParameterizedThreadStart/Program.cs
+++ ParameterizedThreadStart/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ParameterizedThreadStart
 {
@@ -11,6 +12,29 @@ namespace ParameterizedThreadStart
         //Потоки с параметрами и ParameterizedThreadStart
         static void Main(string[] args)
         {
+            int number = 4;
+            //Создаем новый поток
+            Thread myThread = new Thread(Count);
+            myThread.Start(number);
+
+            for(int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i*i);
+                Thread.Sleep(300);
+            }
+        }
+
+        public static void Count(object x)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                int n = (int)x;
+
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i * n);
+                Thread.Sleep(400);
+            }
         }
     }
 }
