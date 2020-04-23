dbd12fac3d20098c62d03b14017ed5a56f111b9b
Nezeeeeee <serezha_knyazev_92@mail.ru>
2020-04-23 20:36:17 +0300

    Синхронизация потоков

---
+++ Thread_Synchronization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+//Синхронизация потоков
+namespace Thread_Synchronization
+{
+    class Program
+    {
+        static int x = 0;
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
+        }
+
+        public static void Count()
+        {
+            x = 1;
+            for(int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                x++;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
